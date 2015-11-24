c473d605a2e0499dff6499d0338e4a6f6815b78d
RANZA <daisyranikesavan@gmail.com>
2015-11-24 15:23:33 +1100

    final commit

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,8 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 
-
-
 //= require underscore-min
 //= require backbone-min
 
@@ -11,7 +9,6 @@
 //= require_tree ./backbone_js/routers
 //= require_tree ./backbone_js/views
 //= require init.js
-
 
 //= require_self
 
